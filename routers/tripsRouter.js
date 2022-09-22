fde60cb466ee977a003f40d0a3c07adb195ee828
starstormfall <photon_dazzles_02@icloud.com>
2022-09-22 11:54:26 +0800

    edited routes more

--- routers/tripsRouter.js
+++ routers/tripsRouter.js
@@ -7,51 +7,54 @@ class TripsRouter {
   }
 
   routes() {
+    // /:TRIPID/USERS/:USERID
+
     // EDITED: CRUD routes for overarching trip retrieval for a particular user, added params
-    router.get("/:userId", this.controller.getAllTrips.bind(this.controller));
+    router.get(
+      "/users/:userId",
+      this.controller.getAllTrips.bind(this.controller)
+    );
     //add trip POST
     // delete trip DELETE
 
-    router.get(
-      "/:userId/:tripId",
-      this.controller.getOneTrip.bind(this.controller)
-    );
+    // GET ALL INFO FROM ONE SINGLE TRIP : info like users
+    router.get("/:tripId", this.controller.getOneTrip.bind(this.controller));
     // edit trip : PUT
 
     //CRUD for packing list
     router.get(
-      "/:userId/:tripId/packing-list",
+      "/:tripId/users/:userId/packing-list",
       this.controller.getAllPackItems.bind(this.controller)
     );
 
     router.post(
-      "/:userId/:tripId/packing-list",
+      "/:tripId/users/:userId/packing-list",
       this.controller.addPackItem.bind(this.controller)
     );
     router.put(
-      "/:userId/:tripId/packing-list",
+      "/:tripId/users/:userId/packing-list",
       this.controller.editPackItem.bind(this.controller)
     );
     router.delete(
-      "/:userId/:tripId/packing-list",
+      "/:tripId/users/:userId/packing-list",
       this.controller.removePackItem.bind(this.controller)
     );
 
     //CRUD for wishlist
     router.get(
-      "/:userId/:tripId/wishlist",
+      "/:tripId/users/:userId/wishlist",
       this.controller.getAllWishlistItems.bind(this.controller)
     );
 
     //CRUD for calendar
     router.get(
-      "/:userId/:tripId/calendar",
+      "/:tripId/users/:userId/calendar",
       this.controller.getAllCalendarItems.bind(this.controller)
     );
 
     //CRUD for comments
     router.get(
-      "/:userId/:tripId/comments",
+      "/:tripId/users/:userId/comments",
       this.controller.getAllComments.bind(this.controller)
     );
 
