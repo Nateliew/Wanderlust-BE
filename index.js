414168bbd220e1b95e6d0001edaf98e28aa07552
root <root@LAPTOP-C6553LAC.localdomain>
2022-09-22 13:34:48 +0800

    updated after merge

--- index.js
+++ index.js
@@ -7,32 +7,34 @@ const { auth } = require("express-oauth2-jwt-bearer");
 require("dotenv").config();
 
 // import routers
-const TripsRouter = require("./routers/tripsRouter");
-const PackItemsRouter = require("./routers/packItemsRouter");
-const UsersRouter = require("./routers/usersRouter");
+// const TripsRouter = require("./routers/tripsRouter");
+// const PackItemsRouter = require("./routers/packItemsRouter");
+// const UsersRouter = require("./routers/usersRouter");
 
 // import controllers
-const TripsController = require("./controllers/tripsController");
-const PackItemsController = require("./controllers/packItemsController");
-const UsersController = require("./controllers/usersController");
+// const TripsController = require("./controllers/tripsController");
+// const PackItemsController = require("./controllers/packItemsController");
+// const UsersController = require("./controllers/usersController");
 
 // import DB
-const db = require("./db/models/index");
+// const db = require("./db/models/index");
 
-const { user, trip, user_trip, item, trip_item, comment, wishlist, calendar } =
-  db;
+// const { user, trip, user_trip, item, trip_item, comment, wishlist, calendar } =
+//   db;
 
-// initialize controllers
-const tripsController = new TripsController(
-  trip,
-  user,
-  user_trip,
-  comment,
-  wishlist,
-  calendar
-);
-const packItemsController = new PackItemsController(packingitem);
-const usersController = new UsersController(user, user_trip);
+// // initialize controllers
+// const tripsController = new TripsController(
+//   trip,
+//   user,
+//   user_trip,
+//   item,
+//   trip_item,
+//   comment,
+//   wishlist,
+//   calendar
+// );
+// const packItemsController = new PackItemsController(item);
+// const usersController = new UsersController(user);
 
 // initialize routers
 // const tripsRouter = new TripsRouter(tripsController).routes();
@@ -48,9 +50,9 @@ app.use(cors());
 // Enable reading JSON request bodies
 app.use(express.json());
 
-app.use("/trips", tripsRouter);
-app.use("/items-catalog", packItemsRouter);
-app.use("/users", usersRouter);
+// app.use("/trips", tripsRouter);
+// app.use("/items-catalog", packItemsRouter);
+// app.use("/users", usersRouter);
 
 app.use(router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
