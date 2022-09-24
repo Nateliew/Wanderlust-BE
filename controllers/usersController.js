b9c7d5bb5a41d6588420e4017802a2bbcfafbc3d
root <root@LAPTOP-C6553LAC.localdomain>
2022-09-24 17:40:20 +0800

    saturday

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,24 @@
 const BaseController = require("./baseController");
 
 class UsersController extends BaseController {
-  constructor(model) {
+  constructor(model, userModel) {
     super(model);
+    this.userModel = userModel;
+  }
+  async insertOneUser(req, res) {
+    console.log("Add user", req.body);
+    const { firstName, lastName, email } = req.body;
+    try {
+      const newUser = await this.userModel.create({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+      });
+      // console.log(user.id);
+      return res.json(newUser);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
   }
 }
 
